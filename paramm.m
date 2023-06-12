f3b3e726e8cd6eeac13dbd12559b180d59cd0fdf
ThomBBaaaa <kkssjj947@gamil.com>
2023-06-12 22:11:20 +0900

    For everyone, Who loves Drone

---
+++ paramm.m
@@ -0,0 +1,24 @@
+m = 0.2;
+K = 1256.637;
+Kn =6.1028207267564559047121275317652e-7;
+Km =6.1028207267564559047121275317652e-7;
+
+J = [0.00031              0              -0.000004657;
+     0               0.00029             0;
+      -0.000004657                     0       0.000243];
+Tau = 0.001 ;
+Jxx = 0.00031;
+ Jyy = 0.00029 ;
+Jzz =  0.000243;
+Jxy=0;
+l = 0.1125;
+g=9.8;
+
+
+x_air_damping_constant = 0.1;
+y_air_damping_constant = 0.1;
+z_air_damping_cosntant = 0.3;
+
+Phi_air_damping_constant = 0;
+Theta_air_damping_constant = 0;
+Psi_air_damping_constant = 0;
